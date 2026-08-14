ac1bf3c8cecb3950dc4441e39ac5cfd4b1824547
Marigoldminibar <ersin.cirisci@marigold.com.tr>
2026-08-14 15:15:45 +0300

    Remove unused UI module and clean service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE='finpocket-v5';
-const ASSETS=['./','./index.html','./style.css','./engine.js','./app.js','./storage.js','./notifications.js','./ui.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./qr.html'];
+const ASSETS=['./','./index.html','./style.css','./engine.js','./app.js','./storage.js','./notifications.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./qr.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
